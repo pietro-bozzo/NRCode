9898ea03589bf318e15a144c7fbcbf261dcbef3b
pietro <pietro.bozzo1998@gmail.com>
2026-06-26 21:14:47 +0200

    new reults: ISA detection box plots, HPC-PFC coupling

--- Python/ISRUtilities.py
+++ Python/ISRUtilities.py
@@ -14,31 +14,32 @@ def paperColors(i,alpha='ff'):
     # IBM colorblind safe palette
     #colors = ["#648fff","#ffb000","#dc267f","#785ef0","#fe6100"] * 3
 
-    colors = {'hpc':       '#87b69e', # 1. HPC
-              'nr':        "#a076b9", # 2. NR
-              'pfc':       "#fc7860", # 3. PFC
-              'th':        "#f4ce32", # 4. TH
-              'sws':       "#ecc8e3", # 5. nREM
-              'rem':       "#a8c7ec", # 6. REM
-              'wake':      "#fff3b6", # 7. wake
-              'on':        "#d8e9bc", # 8. NR "up state"
-              'off':       "#bee5eb", # 9. NR "down state"
-              'ripples':   "#1f99a3", # 10. ripples
-              'deltas':    "#c80000", # 11. deltas
-              'spindles':  "#2a619e", # 12. spindles
-              'shuffle':   "#888888", # 13. shuffle
-              'v1':        "#f4ce32", # 4bis. V1
-              'amy':       "#f4ce32", # 4bis. AMY
-              'lamy':      "#f4ce32", # 4bis. left AMY
-              'ramy':      "#f4ce32", # 4bis. right AMY
-              'lvmpfc':    "#f4ce32", # 4bis. left ventral PFC
-              'rvmpfc':    "#f4ce32", # 4bis. right ventral PFC
-              'ldmpfc':    "#f4ce32", # 4bis. left dorsal PFC
-              'rdmpfc':    "#f4ce32", # 4bis. right dorsal PFC
-              'lvhpc':     "#f4ce32", # 4bis. left ventral HPC
-              'rvhpc':     "#f4ce32", # 4bis. right ventral HPC
-              'ldhpc':     "#f4ce32", # 4bis. left dorsal HPC
-              'rdhpc':     "#f4ce32", # 4bis. right dorsal HPC
+    colors = {'hpc':           '#87b69e', # 1. HPC
+              'nr':            '#a076b9', # 2. NR
+              'pfc':           "#fc7860", # 3. PFC
+              'th':            "#f4ce32", # 4. TH
+              'sws':           "#ecc8e3", # 5. nREM
+              'rem':           "#a8c7ec", # 6. REM
+              'wake':          "#fff3b6", # 7. wake
+              'on':            "#d8e9bc", # 8. NR "up state"
+              'off':           "#bee5eb", # 9. NR "down state"
+              'ripples':       "#1f99a3", # 10. ripples
+              'deltas':        "#c80000", # 11. deltas
+              'spindles':      "#2a619e", # 12. spindles
+              'shuffle':       "#888888", # 13. shuffle
+              'v1':            "#f4ce32", # 4bis. V1
+              'amy':           "#f4ce32", # 4bis. AMY
+              'lamy':          "#f4ce32", # 4bis. left AMY
+              'ramy':          "#f4ce32", # 4bis. right AMY
+              'lvmpfc':        "#f4ce32", # 4bis. left ventral PFC
+              'rvmpfc':        "#f4ce32", # 4bis. right ventral PFC
+              'ldmpfc':        "#f4ce32", # 4bis. left dorsal PFC
+              'rdmpfc':        "#f4ce32", # 4bis. right dorsal PFC
+              'lvhpc':         "#f4ce32", # 4bis. left ventral HPC
+              'rvhpc':         "#f4ce32", # 4bis. right ventral HPC
+              'ldhpc':         "#f4ce32", # 4bis. left dorsal HPC
+              'rdhpc':         "#f4ce32", # 4bis. right dorsal HPC
+              'deltawaves':    "#c80000", # 11bis. deltas
               }
     n_colors = len(colors)
     for j, key in enumerate(tuple(colors)):
@@ -47,7 +48,7 @@ def paperColors(i,alpha='ff'):
     if isinstance(i,int):
         return colors[i % n_colors] + alpha
     elif isinstance(i,str):
-        out = colors[i.lower()] if i in colors else colors['th']
+        out = colors[i.lower()] if i.lower() in colors else colors['th']
         return out + alpha
     else:
         out = []
@@ -55,7 +56,7 @@ def paperColors(i,alpha='ff'):
             if isinstance(j,int):
                 out.append(colors[j % n_colors] + alpha)
             else:
-                o = colors[j.lower()] if j in colors else colors['th']
+                o = colors[j.lower()] if j.lower() in colors else colors['th']
                 out.append(o + alpha)
         return out
 
